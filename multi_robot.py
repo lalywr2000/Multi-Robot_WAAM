8d39e80f13d9e7ba217db035c855e2d0932c1750
lalywr2000 <12190450@inha.edu>
2025-07-24 11:00:40 +0900

    Triple robot system setup

--- multi_robot.py
+++ multi_robot.py
@@ -14,11 +14,15 @@ GCODE_MODE = False
 # True: gcode toolpath   False: manual toolpath
 SCHEDULE_MODE = True
 # True: scheduling       False: visualizing toolpath
-PREPROCESSING_MODE = False
+PREPROCESSING_MODE = True
 # True: preprocessing    False: skip preprocessing
 
 GCODE_PATH  = "./gcode/square.gcode"
 MANUAL_PATH = "./manual/test_1.txt"
+
+ROBOT_COUNT        = 3      # int
+ROBOT_BASEFRAME_R  = 350.0  # float
+ROBOT_HOMEPOS_R    = 250.0  # float
 
 MAX_CONTOUR_LENGTH = 300.0  # float
 
@@ -55,31 +59,54 @@ else:
 agent_models = None
 
 if SCHEDULE_MODE:
-    baseframe1 = np.array([-350.0, 0.0, 0.0])
-    homepos1   = np.array([-250.0, 0.0, 0.0])
+    baseframe1 = np.array([ROBOT_BASEFRAME_R * np.cos(2 * np.pi * 1 / ROBOT_COUNT + np.pi / 6),  # robot1 x_pos
+                           ROBOT_BASEFRAME_R * np.sin(2 * np.pi * 1 / ROBOT_COUNT + np.pi / 6),  # robot1 y_pos
+                           0.0])                                                                 # robot1 z_pos
+    homepos1   = np.array([ROBOT_HOMEPOS_R * np.cos(2 * np.pi * 1 / ROBOT_COUNT + np.pi / 6),
+                           ROBOT_HOMEPOS_R * np.sin(2 * np.pi * 1 / ROBOT_COUNT + np.pi / 6),
+                           0.0])
 
-    baseframe2 = np.array([350.0, 0.0, 0.0])
-    homepos2   = np.array([250.0, 0.0, 0.0])
+    baseframe2 = np.array([ROBOT_BASEFRAME_R * np.cos(2 * np.pi * 3 / ROBOT_COUNT + np.pi / 6),  # robot2 x_pos
+                           ROBOT_BASEFRAME_R * np.sin(2 * np.pi * 3 / ROBOT_COUNT + np.pi / 6),  # robot2 y_pos
+                           0.0])                                                                 # robot2 z_pos
+    homepos2   = np.array([ROBOT_HOMEPOS_R * np.cos(2 * np.pi * 3 / ROBOT_COUNT + np.pi / 6),
+                           ROBOT_HOMEPOS_R * np.sin(2 * np.pi * 3 / ROBOT_COUNT + np.pi / 6),
+                           0.0])
+    
+    baseframe3 = np.array([ROBOT_BASEFRAME_R * np.cos(2 * np.pi * 2 / ROBOT_COUNT + np.pi / 6),  # robot3 x_pos
+                           ROBOT_BASEFRAME_R * np.sin(2 * np.pi * 2 / ROBOT_COUNT + np.pi / 6),  # robot3 y_pos
+                           0.0])                                                                 # robot3 z_pos
+    homepos3   = np.array([ROBOT_HOMEPOS_R * np.cos(2 * np.pi * 2 / ROBOT_COUNT + np.pi / 6),
+                           ROBOT_HOMEPOS_R * np.sin(2 * np.pi * 2 / ROBOT_COUNT + np.pi / 6),
+                           0.0])
 
-    boundingbox = (200.0, 50.0, 300.0)
+    boundingbox = (150.0, 40.0, 100.0)
 
     agent1 = AgentModel(
         base_frame_position = baseframe1,
         home_position       = homepos1,
-        capabilities        = [0,1],
-        velocity            = 50.0,
+        capabilities        = [0,1],  # robot1 material
+        velocity            = 10.0,
         travel_velocity     = 50.0,
         collision_model     = FCLRobotBBCollisionModel(boundingbox, baseframe1),
     )
     agent2 = AgentModel(
         base_frame_position = baseframe2,
         home_position       = homepos2,
-        capabilities        = [0,1],
-        velocity            = 50.0,
+        capabilities        = [0,1],  # robot2 material
+        velocity            = 10.0,
         travel_velocity     = 50.0,
         collision_model     = FCLRobotBBCollisionModel(boundingbox, baseframe2),
     )
-    agent_models = {"robot1": agent1, "robot2": agent2}
+    agent3 = AgentModel(
+        base_frame_position = baseframe3,
+        home_position       = homepos3,
+        capabilities        = [0,1],  # robot3 material
+        velocity            = 10.0,
+        travel_velocity     = 50.0,
+        collision_model     = FCLRobotBBCollisionModel(boundingbox, baseframe3),
+    )
+    agent_models = {"robot1": agent1, "robot2": agent2, "robot3": agent3}
 
 
 #------------------ Preprocessing -----------------
@@ -127,7 +154,7 @@ if SCHEDULE_MODE:
 
 if SCHEDULE_MODE:
     animate_multi_agent_toolpath_full(
-        toolpath, schedule, agent_models, limits=((-550, 550), (-250, 250))
+        toolpath, schedule, agent_models, limits=((-550, 550), (-430, 250))
     )
 
 else:
